3ed0c5e2b59af6598bb59c2104303a901f75db02
Krish Asher <krishda2@illinois.edu>
2022-09-24 01:43:13 -0500

    Complete project, data in json

--- project.py
+++ project.py
@@ -5,6 +5,9 @@ import time
 import datetime
 from praw.models import MoreComments
 from keys import *
+import json
+
+output = {}
 
 #requests used to connect with Reddit API
 
@@ -16,9 +19,14 @@ reddit = praw.Reddit(
     username = "krishasher",
 )
 
+#user input
+subred = str(input("What is the subreddit name\n"))
+while subred[0]=="r" and subred[1]=="/":
+    print("Do not include /r in the string")
+
 #This requests gives the access token which is used in the header below under 'Authorization'
 #The above code can be deleted once the acess token is found
-sr = reddit.subreddit('funny')
+sr = reddit.subreddit(subred)
 
 #dictionary is used to store top 5 submissions for the last 26 weeks
 dict_time = {}
@@ -28,30 +36,39 @@ for i in range(26):
 #The for loop below looks through each submission and adds it to the dictionary
 #depending on the week from today's date it was created.  The nested for loop
 #takes the six top comments of the submission  
-for submission in reddit.subreddit("funny").top(time_filter="all", limit = None):
-    for comment in submission.comments[0:6]:
-        if isinstance(comment, MoreComments):
-            continue
-        print(comment.body)
-
+sub_comment = []
+for submission in reddit.subreddit(subred).top(time_filter="all", limit = None):
+    new_dict = {'submission': submission.title,
+                'up-votes': submission.score}
     t = (time.time() - submission.created_utc)//604800
     if (t < 26):
-        dict_time[t].append(submission)
+        for comment in submission.comments[0:6]:
+            if isinstance(comment, MoreComments):
+                continue
+            sub_comment.append(comment.body)
+        new_dict['comments'] = sub_comment
+        dict_time[t].append(new_dict)
 
+output['subreddit_info'] = dict_time
+
+print("Done")
 #This finds all the users in the subreddit and adds them to the dictionary
 #if they are less than three months old
 users = {}
-for comment in sr.comments(limit=1000):
-     auth = comment.author
-     if not auth in users and time.time()-auth.created_utc<54432000:
-         users[auth] = auth.created_utc
+for comment in sr.comments(limit=50):
+    auth = comment.author
+    if auth:
+        auth_d = auth.created_utc
+        if not auth in users and time.time()-auth_d<54432000:
+            users[str(auth)] = auth_d
+output['subreddit_users'] = users
 
-print(dict_time[0])
+output['subbreddit_user_count'] = len(users)
 
+json_object = json.dumps(output, indent=4)
 
-
-
-
-for i in range(26):
-    print(i , dict_time[i])
+#Putting into json file
+with open('./reddit-subreddit-scraper/output.json', 'w') as f:
+    f.write(json_object)
+    print("New json file is created from data.json file")
     
